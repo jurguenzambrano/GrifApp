2b6ee8585163bb5d7b6e4ff4a96c5f58cc9d6dce
Jurguen Zambrano <jurguenzambrano@gmail.com>
2017-05-01 18:10:38 -0500

    Pintado de marcadores de los grifos

--- app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
+++ app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
@@ -2,39 +2,26 @@ package pe.edu.upc.grifapp.activities;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
-import com.androidnetworking.interfaces.JSONObjectRequestListener;
-import com.androidnetworking.interfaces.ParsedRequestListener;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
@@ -48,10 +35,8 @@ import pe.edu.upc.grifapp.GrifApp;
 import pe.edu.upc.grifapp.R;
 import pe.edu.upc.grifapp.models.Fuel;
 import pe.edu.upc.grifapp.models.Login;
-import pe.edu.upc.grifapp.models.Message;
 import pe.edu.upc.grifapp.models.User;
 import pe.edu.upc.grifapp.network.FuelStationApi;
-import pe.edu.upc.grifapp.network.UsersApi;
 
 public class FuelStationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
