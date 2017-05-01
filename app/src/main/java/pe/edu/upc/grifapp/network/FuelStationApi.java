d38d42208b4038e477055d6094a29b394104c3b7
Juan <juan.botetano@gmail.com>
2017-05-01 10:44:07 -0500

    Actualizando a la ultima version

---
+++ app/src/main/java/pe/edu/upc/grifapp/network/FuelStationApi.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.grifapp.network;
+
+import pe.edu.upc.grifapp.models.Fuel;
+
+/**
+ * Created by Juan on 1/05/2017.
+ */
+
+public class FuelStationApi {
+    public static String FUEL_URL = "http://api.fuel.maraquya.com/fuel";
+    public Fuel fuel;
+
+    public Fuel getCurrentFuel() {
+        return fuel;
+    }
+
+    public void setCurrentFuel(Fuel fuel) {
+        this.fuel = fuel;
+    }
+}
