90af99828b0b026b587f8b9d655da7f0d853d9e6
Jurguen Zambrano <jurguenzambrano@gmail.com>
2017-05-01 18:08:20 -0500

    Pintado de marcadores de los grifos

--- app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
+++ app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
@@ -1,13 +1,24 @@
 package pe.edu.upc.grifapp.activities;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
@@ -15,12 +26,15 @@ import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.androidnetworking.interfaces.ParsedRequestListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
@@ -46,36 +60,35 @@ public class FuelStationActivity extends AppCompatActivity implements OnMapReady
     Login login;
     List<Fuel> fuels;
 
+    final static int PERMISSIONS_REQUEST_ACCESS_LOCATION = 100;
+    private String locationProvider = LocationManager.GPS_PROVIDER;
+    private boolean locationPermissionGranted = false;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location location;
+    private GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_station);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         fuels = new ArrayList<Fuel>();
-/*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
 
         user = GrifApp.getInstance().getCurrentUser();
         login = GrifApp.getInstance().getCurrentLogin();
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_container);
+        setupLocationUpdates();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_container);
         mapFragment.getMapAsync(this);
 
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+        map = googleMap;
+
         // Obtenemos todas las estaciones de servicio
         AndroidNetworking.get(FuelStationApi.FUEL_URL)
                 .addHeaders("token",login.getToken())
@@ -83,7 +96,6 @@ public class FuelStationActivity extends AppCompatActivity implements OnMapReady
                 .setPriority(Priority.HIGH)
                 .setTag(TAG)
                 .build()
-                //.getAsJSONObject(new JSONObjectRequestListener() {
                 .getAsJSONArray(new JSONArrayRequestListener() {
                     @Override
                     public void onResponse(JSONArray response) {
@@ -97,13 +109,6 @@ public class FuelStationActivity extends AppCompatActivity implements OnMapReady
                                     .position(estacion)
                                     .title(fuels.get(i).getName()));
                         }
-
-                        LatLng upc = new LatLng(-12.0919393,-76.9675561);
-                        CameraPosition cameraPosition = CameraPosition.builder()
-                                .target(upc)
-                                .zoom(16)
-                                .build();
-                        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                     }
 
                     @Override
@@ -121,4 +126,63 @@ public class FuelStationActivity extends AppCompatActivity implements OnMapReady
                 });
 
     }
+
+    private void setupLocationUpdates() {
+        validatePermissions();
+        if (locationPermissionGranted) {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    refreshCurrentLocation(location);
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                public void onProviderEnabled(String provider) {
+                }
+
+                public void onProviderDisabled(String provider) {
+                }
+            };
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+            location = locationManager.getLastKnownLocation(locationProvider);
+            refreshCurrentLocation(location);
+        }
+    }
+
+    private void validatePermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST_ACCESS_LOCATION);
+            }
+        } else {
+            locationPermissionGranted = true;
+        }
+    }
+
+    private void refreshCurrentLocation(Location location) {
+        String locationDescription = "======================> Latitude: " + String.valueOf(location.getLatitude()) + " Longitude: " + String.valueOf(location.getLongitude());
+        Log.d(TAG, locationDescription);
+
+        LatLng currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        CameraPosition cameraPosition = CameraPosition.builder()
+                .target(currentLocation)
+                .zoom(16)
+                .build();
+        map.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+    }
+
 }
