a934eb643df6aade543ec7bdc403686abf45d2b1
Jurguen Zambrano <jurguenzambrano@gmail.com>
2017-05-01 14:21:30 -0500

    Pintado de marcadores de los grifos

--- app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
+++ app/src/main/java/pe/edu/upc/grifapp/activities/FuelStationActivity.java
@@ -6,13 +6,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.androidnetworking.interfaces.ParsedRequestListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,7 +39,7 @@ import pe.edu.upc.grifapp.models.User;
 import pe.edu.upc.grifapp.network.FuelStationApi;
 import pe.edu.upc.grifapp.network.UsersApi;
 
-public class FuelStationActivity extends AppCompatActivity {
+public class FuelStationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static String TAG = "GrifApp";
     User user;
@@ -39,8 +50,8 @@ public class FuelStationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuel_station);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         fuels = new ArrayList<Fuel>();
 /*
@@ -57,36 +68,42 @@ public class FuelStationActivity extends AppCompatActivity {
         user = GrifApp.getInstance().getCurrentUser();
         login = GrifApp.getInstance().getCurrentLogin();
 
-        // Obtenemos los datos del usuario
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_container);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        // Obtenemos todas las estaciones de servicio
         AndroidNetworking.get(FuelStationApi.FUEL_URL)
                 .addHeaders("token",login.getToken())
                 .addHeaders("id",user.getId())
                 .setPriority(Priority.HIGH)
                 .setTag(TAG)
                 .build()
-                .getAsJSONObject(new JSONObjectRequestListener() {
+                //.getAsJSONObject(new JSONObjectRequestListener() {
+                .getAsJSONArray(new JSONArrayRequestListener() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            if(!response.getString("code").equals("0")) {
-                                //Snackbar.make(linearLayout, response.getString("message"), Snackbar.LENGTH_LONG).show();
-                                return;
-                            }
+                    public void onResponse(JSONArray response) {
+                        //message = Message.build(response);
+                        fuels = Fuel.build(response);
 
-                            //message = Message.build(response);
-                            fuels = Fuel.build(response.optJSONArray("data"));
-
-                            //if (message.getCode().equals("0")) {
-                                // Si todo esta ok, mostramos mapa de estaciones de servicio cercanos
-                                /*
-                            }else{
-                                //Snackbar.make(linearLayout, message.getMessage(), Snackbar.LENGTH_LONG).show();
-                                return;
-                            }*/
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                            return;
+                        int length = fuels.size();
+                        for(int i = 0; i < length; i++) {
+                            LatLng estacion = new LatLng(Double.parseDouble(fuels.get(i).getAltitude()),Double.parseDouble(fuels.get(i).getLatitude()));
+                            googleMap.addMarker(new MarkerOptions()
+                                    .position(estacion)
+                                    .title(fuels.get(i).getName()));
                         }
+
+                        LatLng upc = new LatLng(-12.0919393,-76.9675561);
+                        CameraPosition cameraPosition = CameraPosition.builder()
+                                .target(upc)
+                                .zoom(16)
+                                .build();
+                        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                     }
 
                     @Override
@@ -102,6 +119,6 @@ public class FuelStationActivity extends AppCompatActivity {
                         return;
                     }
                 });
-    }
 
+    }
 }
